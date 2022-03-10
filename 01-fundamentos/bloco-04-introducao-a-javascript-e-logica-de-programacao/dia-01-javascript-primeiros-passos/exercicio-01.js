80b1250c24d36c55d5173f1c600c304022b85a79
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-03-10 04:13:29 -0300

    Exercício 01 concluído

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-01.js
@@ -0,0 +1,30 @@
+// Faça cinco programas, um para cada operação aritmética básica.
+// Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+//      Adição (a + b)
+//      Subtração (a - b)
+//      Multiplicação (a * b)
+//      Divisão (a / b)
+//      Módulo (a % b)
+
+let a = 10;
+let b = 2;
+
+// Adição
+resultado = a + b;
+console.log('O resultado de ' + a + ' mais ' + b + ' é igual a: ' + resultado);
+
+// Substração
+resultado = a - b;
+console.log('O resultado de ' + a + ' menos ' + b + ' é igual a: ' + resultado);
+
+// Multiplicação
+resultado = a * b;
+console.log('O resultado de ' + a + ' vezes ' + b + ' é igual a: ' + resultado);
+
+// Divisão
+resultado = a / b;
+console.log('O resultado de ' + a + ' dividido por ' + b + ' é igual a: ' + resultado);
+
+// Módulo
+resultado = a % b;
+console.log('O resto da divisão de ' + a + ' dividido por ' + b + ' é igual a: ' + resultado);
