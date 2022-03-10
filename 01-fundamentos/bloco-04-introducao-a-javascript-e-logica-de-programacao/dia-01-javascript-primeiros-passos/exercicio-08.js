083e85fa5988457ea72f4c718095ccda4d57a103
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-03-10 19:05:26 -0300

    Feito correção no Exercício 08 e Concluído o Exercício 09

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-08.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-08.js
@@ -1,6 +1,6 @@
 // Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
 //     Bonus: use somente um if .
 
-const numeros = [31, 20, 11];
-let resultado = numeros[1] % 2 === 0 || numeros[2] % 2 === 0 || numeros[3] % 2 === 0;
+const numeros = [31, 21, 11];
+let resultado = numeros[0] % 2 === 0 || numeros[1] % 2 === 0 || numeros[2] % 2 === 0;
 console.log(resultado);
