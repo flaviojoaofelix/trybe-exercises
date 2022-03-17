b7b175398329528c6f38f3ad0f41e434b9cfb7b0
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-03-17 10:08:56 -0300

    Exercício 01 - Questão 06 - Concluído

--- 01-fundamentos/bloco-05-javascript-dom-evento-e-web-storage/dia-03-javascript-eventos/exercicio-01.js
+++ 01-fundamentos/bloco-05-javascript-dom-evento-e-web-storage/dia-03-javascript-eventos/exercicio-01.js
@@ -128,5 +128,28 @@ function changeTextFriday() {
 
     }
 }
+
 buttonEvent('btn-friday', changeTextFriday);
 
+// Questão 06
+
+// Implemente duas funções que criem um efeito de "zoom".
+// Ao passar o ponteiro do mouse em um dia do mês no calendário, o texto desse dia deve aumentar e,
+// quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original.
+//   Dica - Propriedade: event.target .
+
+function dayZoom() {
+    days = document.querySelectorAll('.day')
+    for (let i = 0; i < days.length; i += 1) {
+        days[i].addEventListener('mouseover', dayZoomOver);
+        days[i].addEventListener('mouseout', dayZoomOut);
+    }
+}
+function dayZoomOver(day) {
+    day.target.style.fontSize = '40px';
+}
+function dayZoomOut(day) {
+    day.target.style.fontSize = '20px';
+}
+
+dayZoom();
