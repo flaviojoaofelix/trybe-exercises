4f1ebccebf1ce903504c90a68d689137bbc9afe8
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-03-16 21:36:18 -0300

    Exercício 01 - Questão 01 - Concluído

--- 01-fundamentos/bloco-05-javascript-dom-evento-e-web-storage/dia-03-javascript-eventos/exercicio-01.js
+++ 01-fundamentos/bloco-05-javascript-dom-evento-e-web-storage/dia-03-javascript-eventos/exercicio-01.js
@@ -9,8 +9,51 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
-  };
+};
   
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+// Escreva seu código abaixo.
+
+// Questão 01
+
+// O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro.
+// Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul>
+// com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e
+// Segunda-feira.
+//  Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+//  Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+//  Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezDaysHolidays = [24, 25, 31];
+const dezDaysFridays = [4, 11, 18, 25];
+
+let daysList = document.getElementById('days');
+
+function createDays(days, fridays, holidays) {
+    for (let i = 0; i < days.length; i += 1) {
+        let day = document.createElement('li');
+        day.innerText = days[i];
+        day.className = 'day';
+
+        if(verifyFridays(days[i], fridays)) day.classList.add('friday');
+        if(verifyHolidays(days[i], holidays)) day.classList.add('holiday');
+    
+        daysList.appendChild(day);
+    }
+}
+function verifyHolidays(day, holidays) {
+    for (let holiday of holidays) {
+        if (holiday === day) return true;
+    }
+    return false;
+}
+function verifyFridays(day, fridays) {
+    for (let friday of fridays) {
+        if (friday === day) return true;
+    }
+    return false;
+}
+
+createDays(dezDaysList, dezDaysFridays, dezDaysHolidays);
