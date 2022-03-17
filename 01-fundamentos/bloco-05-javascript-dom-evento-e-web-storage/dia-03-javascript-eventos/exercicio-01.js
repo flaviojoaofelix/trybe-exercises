13e83b8fa1089296166fe9df6d4bd5c71d08d979
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-03-17 10:23:32 -0300

    Exercício 01 - Questão 08 - Concluído

--- 01-fundamentos/bloco-05-javascript-dom-evento-e-web-storage/dia-03-javascript-eventos/exercicio-01.js
+++ 01-fundamentos/bloco-05-javascript-dom-evento-e-web-storage/dia-03-javascript-eventos/exercicio-01.js
@@ -162,7 +162,7 @@ dayZoom();
 //   O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
 
 function createTask(task) {
-    parent = document.querySelector('.my-tasks');
+    let parent = document.querySelector('.my-tasks');
     let taskTag = document.createElement('span');
     taskTag.innerText = task;
 
@@ -172,8 +172,20 @@ function createTask(task) {
 createTask('Cozinhar');
 
 // Questão 08:
+
 // Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior.
 // Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento
 // de tag <div> com a classe task .
 //   O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
 //   O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+function createTaskCaption(color) {
+    let parent = document.querySelector('.my-tasks');
+    let taskCaption = document.createElement('div');
+    taskCaption.className = 'task';
+    taskCaption.style.backgroundColor = color;
+
+    parent.appendChild(taskCaption);
+}
+
+createTaskCaption('yellow');
