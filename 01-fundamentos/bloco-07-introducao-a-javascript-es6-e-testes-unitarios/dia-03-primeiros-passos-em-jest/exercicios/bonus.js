6ac1d440af04babc3e3aa3d876157f3d54f4b767
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-04-06 12:06:43 -0300

    Exercício Bônus - Concluído

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/bonus.js
@@ -0,0 +1,63 @@
+// Dados
+const professionalBoard = [
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+
+// Pesquisa
+const searchEmployee = (id, detail) => {
+  let employee = '';
+  
+  for (let i = 0; i < professionalBoard.length; i += 1) {
+    let searchEmployee = professionalBoard[i];
+
+    if (searchEmployee.id === id) {
+      employee = searchEmployee;
+    }
+  }
+
+  if (!employee) {
+    throw new Error(`ID não encontrado!`);
+  }
+  if (!employee[detail]) {
+    throw new Error(`Informação não encontrada!`);
+  }
+
+  return employee[detail];
+};
+
+module.exports = searchEmployee;
