58ec880462110d120d1284dcfd217137aa2c0a48
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-04-08 15:31:45 -0300

    Exercício Parte 03 - Concluído

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-fluxo-de-excecao-e-objetos/exercicio-parte-03.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-fluxo-de-excecao-e-objetos/exercicio-parte-03.js
@@ -17,3 +17,56 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+// Questão 01
+const addShift = (lesson, key, shift) => {
+  lesson[key] = shift;
+  return lesson;
+};
+// console.log(addShift(lesson2, 'turno', 'noite'));
+
+// Questão 02
+const listLessonKeys = (lesson) => Object.keys(lesson);
+// console.log(listLessonKeys(lesson1));
+
+// Questão 03
+const lessonSize = (lesson) => Object.keys(lesson).length
+// console.log(lessonSize(lesson3));
+
+// Questão 04
+const lessonValues = (lesson) => Object.values(lesson)
+// console.log(lessonValues(lesson1));
+
+// Questão 05
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+// console.log(allLessons);
+
+// Questão 06
+const getStudentsNumber = (obj) => {
+  const lessons = Object.keys(obj);
+  let studentsNumber = 0;
+
+  for (let i = 0; i < lessons.length; i += 1) {
+    studentsNumber += obj[lessons[i]].numeroEstudantes;
+  }
+
+  return studentsNumber;
+};
+// console.log(getStudentsNumber(allLessons));
+
+// Questão 07
+const getKeyValue = (lesson, number) => Object.values(lesson)[number];
+// console.log(getKeyValue(lesson2, 2));
+
+// Questão 08
+const verifyKeyValue = (lesson, key, value) => {
+  const entries = Object.entries(lesson);
+  let answer = false;
+
+  for (let i = 0; i < entries.length; i += 1) {
+    if(entries[i][0] === key && entries[i][1] === value) answer = true;
+  }
+
+  return answer;
+};
+console.log(verifyKeyValue(lesson1, 'turno', 'manhã'));
