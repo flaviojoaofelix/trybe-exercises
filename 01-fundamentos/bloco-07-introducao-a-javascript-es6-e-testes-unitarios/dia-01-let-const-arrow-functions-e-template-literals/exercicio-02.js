7cc5257c7eff2eca2944c328b4d795f6e670e9d9
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-04-10 22:16:57 -0300

    Exercício Parte 02 - Questão 02 - Concluído

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-let-const-arrow-functions-e-template-literals/exercicio-02.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-let-const-arrow-functions-e-template-literals/exercicio-02.js
@@ -16,3 +16,22 @@ console.log(factorial(10));
 
 const recursiveFactorial = (number) => number > 1 ? number * recursiveFactorial(number - 1) : 1;
 console.log(recursiveFactorial(10));
+
+// Questão 02
+
+const longWord = (text) => {
+  let arr = text.split(' ');
+  let long = 0
+  let word = '';
+
+  for (i = 0; i < arr.length; i += 1) {
+    if (arr[i].length > long ) {
+      long = arr[i].length;
+      word = arr[i];
+    }
+  }
+
+  return word;
+};
+
+console.log(longWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
