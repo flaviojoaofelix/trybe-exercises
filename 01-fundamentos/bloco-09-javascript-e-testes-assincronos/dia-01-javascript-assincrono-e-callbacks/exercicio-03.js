b6de3ff25ed7a76f88f687949dee669fbf32fead
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-04-21 17:55:28 -0300

    Diversas alterações

---
+++ 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicio-03.js
@@ -0,0 +1,15 @@
+// Exercício 03
+
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars.name);
+};
+
+// getPlanet(); // imprime Marte depois de 4 segundos
+setTimeout(() => getPlanet(), 4000);
---
+++ 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/fetch-exemplo-01.js
@@ -0,0 +1,25 @@
+// Fetch API - Exemplo 01
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   console.log(fetch(url));
+// }
+
+// fetchJoke();
+
+// Resposta: Promise { <pending> }
+
+const fetch = require('node-fetch');
+
+const fetchJoke = () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value));
+}
+
+fetchJoke();
