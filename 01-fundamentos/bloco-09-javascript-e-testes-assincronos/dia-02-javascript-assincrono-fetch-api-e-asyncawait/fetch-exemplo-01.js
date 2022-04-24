439b3862082ae026e1a82d1a630df7c1a5ef3452
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-04-24 18:35:37 -0300

    Fetch API - Exemplo 01 - Concluído

--- 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/fetch-exemplo-01.js
+++ 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/fetch-exemplo-01.js
@@ -12,14 +12,31 @@
 
 // Resposta: Promise { <pending> }
 
+//////////////////////////////////// Solução 02
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value));
+// }
+
+// fetchJoke();
+
+/////////////////////////////////// Solução Final
+
 const fetch = require('node-fetch');
 
 const fetchJoke = () => {
-  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+  const url = 'api.chucknorris.io/jokes/random?category=dev';
 
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data.value));
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
 }
 
 fetchJoke();
