4c13e4691c3255f5d46f3375b1d2f31754939758
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-08-22 16:28:32 -0300

    Conteúdo: Listando times através do método GET - Cadastrando times através do método POST

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/soccer-team-manager/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/soccer-team-manager/src/app.js
@@ -20,4 +20,11 @@ app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam});
+});
+
 module.exports = app;
