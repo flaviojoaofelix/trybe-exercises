07198030d4c4ed3ade06b06c590edc50ca59feb3
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-08-22 21:26:12 -0300

    Conteúdo: Exercícios - 04

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/my-activities/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/my-activities/src/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const activities = require('./data');
 
 const app = express();
+
+app.get('/myActivities/:id', (req, res) => {
+  const activity = activities.find((act) => act.id === Number(req.params.id));
+
+  res.status(200).json(activity);
+});
 
 module.exports = app;
