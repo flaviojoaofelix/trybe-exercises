c7070e123ee8200cd91884803868f4ce510c1253
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-08-22 21:35:04 -0300

    Conteúdo: Exercícios - 06

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/my-activities/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/my-activities/src/app.js
@@ -13,4 +13,15 @@ app.get('/myActivities/:id', (req, res) => {
   res.status(200).json(activity);
 });
 
+app.get('/filter/myActivities', (req, res) => {
+  const { status } = req.query;
+  let filtered = activities;
+
+  if (status) {
+    filtered = activities.filter((act) => act.status === status);
+  }
+
+  res.status(200).json(filtered);
+});
+
 module.exports = app;
