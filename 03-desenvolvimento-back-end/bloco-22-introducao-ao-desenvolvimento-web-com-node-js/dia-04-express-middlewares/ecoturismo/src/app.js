9f72a3d08e1fc4dee515475aba4472a42583bc14
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-08-24 21:07:38 -0300

    Conteúdo: Exercícios - Agora a Prática  - 01/06

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-express-middlewares/ecoturismo/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-express-middlewares/ecoturismo/src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
+const validateDescription = require('./middlewares/validateDescription');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', validatePrice, validateName, (_req, res) => {
-  res.status(201).json({ message: 'Atividade registrada com sucesso!' });
+app.post('/activities',
+  validateDescription,
+  validatePrice,
+  validateName,
+  (_req, res) => {
+    res.status(201).json({ message: 'Atividade registrada com sucesso!' });
 });
 
 module.exports = app;
