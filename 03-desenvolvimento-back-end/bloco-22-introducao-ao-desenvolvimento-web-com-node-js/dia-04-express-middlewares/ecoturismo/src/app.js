2e98eef217b93ede55fed8a21c3cb4a4e429eb02
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-08-24 21:18:42 -0300

    Conteúdo: Exercícios - Agora a Prática  - 01/08

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-express-middlewares/ecoturismo/src/app.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-express-middlewares/ecoturismo/src/app.js
@@ -3,12 +3,14 @@ const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateRating = require('./middlewares/validateRating');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
+  validateRating,
   validateCreatedAt,
   validateDescription,
   validatePrice,
