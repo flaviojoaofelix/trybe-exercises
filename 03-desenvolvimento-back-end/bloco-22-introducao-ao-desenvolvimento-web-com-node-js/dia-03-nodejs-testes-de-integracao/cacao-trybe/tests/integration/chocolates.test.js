dc0202f33221b32cc22a45d2f12ab019dadcb5b8
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-09-07 17:42:39 -0300

    build: Cacao Trybe API - Dublês de Teste

--- 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-nodejs-testes-de-integracao/cacao-trybe/tests/integration/chocolates.test.js
+++ 03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-nodejs-testes-de-integracao/cacao-trybe/tests/integration/chocolates.test.js
@@ -1,4 +1,6 @@
 const chai = require('chai');
+const sinon = require('sinon');
+const fs = require('fs');
 const chaiHttp = require('chai-http');
 const app = require('../../src/app');
 
@@ -6,7 +8,53 @@ const { expect } = chai;
 
 chai.use(chaiHttp);
 
+const mockFile = JSON.stringify({
+  brands: [
+    {
+      id: 1,
+      name: 'Lindt & Sprungli',
+    },
+    {
+      id: 2,
+      name: 'Ferrero',
+    },
+    {
+      id: 3,
+      name: 'Ghirardelli',
+    },
+  ],
+  chocolates: [
+    {
+      id: 1,
+      name: 'Mint Intense',
+      brandId: 1,
+    },
+    {
+      id: 2,
+      name: 'White Coconut',
+      brandId: 1,
+    },
+    {
+      id: 3,
+      name: 'Mon Chéri',
+      brandId: 2,
+    },
+    {
+      id: 4,
+      name: 'Mounds',
+      brandId: 3,
+    },
+  ]});
+
 describe('Testando a API Cacao Trybe', function () {
+  beforeEach(function () {
+    sinon.stub(fs.promises, 'readFile')
+      .resolves(mockFile);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
   describe('Usando o método GET em /chocolates', function () {
     it('Retorna a lista completa de chocolates!', async function () {
       const output = [
