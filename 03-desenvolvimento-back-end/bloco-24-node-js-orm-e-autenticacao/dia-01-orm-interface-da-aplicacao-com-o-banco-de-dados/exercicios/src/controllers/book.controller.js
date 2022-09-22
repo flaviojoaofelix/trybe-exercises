605a1684eafef06a6c58bad38d9ac20233b01792
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-09-22 16:50:45 -0300

    :sparkles: feat: exercicio 07 - create getById method at book controller

--- 03-desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/controllers/book.controller.js
+++ 03-desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/controllers/book.controller.js
@@ -1,15 +1,32 @@
 const bookService = require('../services/book.service');
 
+const error500Message = 'Algo deu errado';
+
 const getAll = async (_req, res) => {
   try {
     const books = await bookService.getAll();
     return res.status(200).json(books);
   } catch (e) {
     console.log(e.message);
-    res.status(500).json({ message: 'Algo deu errado' });
+    res.status(500).json({ message: error500Message });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await bookService.getById(id);
+  
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+
+    return res.status(200).json(book);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
   }
 };
 
 module.exports = {
   getAll,
+  getById,
 };
