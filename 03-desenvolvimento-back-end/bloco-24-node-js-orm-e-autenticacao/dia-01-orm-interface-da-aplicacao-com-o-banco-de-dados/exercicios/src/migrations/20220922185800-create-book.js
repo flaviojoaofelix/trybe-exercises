692dbafb5472764fb099707760ce27e421672973
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-09-22 16:08:20 -0300

    :sparkles: feat: exercicio 01 - create book migration

---
+++ 03-desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/migrations/20220922185800-create-book.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      page_quantity: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('books');
+  }
+};
