af181b1b1c1002cd5f61be09bca804ed123cc13b
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-09-22 16:15:31 -0300

    :sparkles: feat: exercicio 04 - create book controller with getall method

---
+++ 03-desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/controllers/book.controller.js
@@ -0,0 +1,11 @@
+const bookService = require('../services/book.service');
+
+const getAll = async (_req, res) => {
+  try {
+    const books = await bookService.getAll();
+    return res.status(200).json(books);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  }
+};
