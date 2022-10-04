47105f9fc1f01bc56d8ce0616943a7d49290afc7
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-10-04 19:28:19 -0300

    Exercício - Agora, a prática: Exercício 03

---
+++ 03-desenvolvimento-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/my-ts-scripts/mass.ts
@@ -0,0 +1,11 @@
+function convert(value: number, fromUnit: string, toUnit: string): number {
+  const units = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
+
+  const fromIndex = units.indexOf(fromUnit);
+  const toIndex = units.indexOf(toUnit);
+  const exponent = (toIndex - fromIndex);
+
+  return value * Math.pow(10, exponent);
+}
+
+console.log(convert(1, 'kg', 'g'));
