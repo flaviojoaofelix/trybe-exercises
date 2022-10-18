911b98f86bb0ec8868c1191d5368247f15236b4b
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-10-18 09:09:02 -0300

    Exercícios - Agora, a prática: Exercício 01 - 01

--- 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicios/escola.ts
+++ 03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicios/escola.ts
@@ -10,4 +10,42 @@ class pessoaEstudante {
     this._notasProva = [];
     this._notasTrabalho = [];
   }
+
+  set matricula(numeroDaMatricula: number) {
+    this._matricula = numeroDaMatricula;
+  }
+
+  get matricula(): number {
+    return this._matricula;
+  }
+
+  set nome(novoNome: string) {
+    this._nome = novoNome;
+  }
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  set notasProva(notas: number[]) {
+    if (notas.length > 4) {
+      throw new Error('O sistema permite no máximo 4 notas de prova');
+    }
+    this._notasProva = notas;
+  }
+
+  get notasProva(): number[] {
+    return this._notasProva;
+  }
+
+  set notasTrabalho(notas: number[]) {
+    if (notas.length > 4) {
+      throw new Error('O sistema permite apenas 2 notas de trabalhos');
+    }
+    this._notasTrabalho = notas;
+  }
+
+  get notasTrabalho(): number[] {
+    return this._notasTrabalho;
+  }
 }
